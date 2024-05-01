e5d3a160482a766ac8bb13ba2389d45c94ac46fb
JOAOMATTA <joao.matta@outlook.com>
2024-05-01 07:47:21 -0300

    Operators and Types of Variables

---
+++ types-variables/src/Operators.java
@@ -0,0 +1,68 @@
+public class Operators {
+    public static void main(String[] args) {
+        //Incrimentador
+        int number = 5;
+        number++;
+        
+        System.out.println(number);
+        System.out.println(++number);
+
+        boolean variable = true;
+
+        System.out.println(!variable);
+
+        // classe Operadores.java
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+        String resultado = (a==b) ? "verdadeiro" : "false";
+
+        System.out.println(resultado);
+
+        //classe Operadores.java
+        int numero1 = 1;
+        int numero2 = 2;
+
+        if(numero1 > numero2)
+            System.out.print("Numero 1 maior que numero 2");
+
+        if(numero1 < numero2)
+            System.out.print("Numero 1 menor que numero 2");
+
+        if(numero1 >= numero2)
+            System.out.print("Numero 1 maior ou igual que numero 2");
+
+        if(numero1 <= numero2)
+            System.out.print("Numero 1 menor ou igual que numero 2");
+
+        if(numero1 != numero2)
+            System.out.print("Numero 1 é diferente de numero 2");
+        
+        //Comparação entre objetos diferentes
+        String name1 = "Teste";
+        String name2 = "Teste2";
+
+        System.out.println(name1.equals(name2));
+
+        // Operadores.java
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+
+        /* Aqui estamos utilizando o operador lógico E para fazer a união de duas 
+        expressões. 
+        É como se estivesse escrito:
+        "Se Condicao1 e Condicao2 forem verdadeiras, executar código"
+        */
+
+        if(condicao1 && condicao2)
+            System.out.print("Os dois valores precisam ser verdadeiros");;
+
+        //Se condicao1 OU condicao2 for verdadeira, executar código.
+        if(condicao1 || condicao2)
+            System.out.print("Um dos valores precisa ser verdadeiro");
+    }
+}
--- types-variables/src/TypesVariables.java
+++ types-variables/src/TypesVariables.java
@@ -1,5 +1,18 @@
 public class TypesVariables {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args) {
+		byte age = 123;
+		short year = 2021;
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salary = 1275.33;
+
+        int number = 10;
+
+        number = 15;
+
+        final double VALOR_DE_PI = 3.14;
+
+
+	}
 }
